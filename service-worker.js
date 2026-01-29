71330ed9f46bb15e73037442f8d3ecd7cb44a5a0
nikhiljoshiji0309-code <nikhil.joshi.ji0309@gmail.com>
2026-01-29 09:25:05 +0530

    Update Firebase configuration in service worker

--- service-worker.js
+++ service-worker.js
@@ -2,17 +2,21 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  projectId: "YOUR_PROJECT_ID",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAxbBOThZxG5W_R1i0zVmh-TxzWOE1uDhM",
+  authDomain: "cosmic-notes-f7648.firebaseapp.com",
+  projectId: "cosmic-notes-f7648",
+  messagingSenderId: "70656136344",
+  appId: "1:70656136344:web:640193b83f63cac569aced"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(payload => {
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
-    icon: "/icon-192.png"
-  });
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: "/icon-192.png"
+    }
+  );
 });
